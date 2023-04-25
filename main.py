87fd429d3e0601ffe87e9c358d7b48afed586121
azmideliaslan <azmideliaslan@gmail.com>
2023-04-25 15:34:24 +0300

    just title can post

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
-import json
+import tweepy
 
-# OAuth2 access token
-access_token = "dS0dChIGxjE17GL-9MClHorAntWimIdklS9EWYeWo48"
+# OAuth2 producthunt access token
+access_token_producthunt = "dS0dChIGxjE17GL-9MClHorAntWimIdklS9EWYeWo48"
 
+# API anahtarları ve erişim belirteçlerinizi buraya girin
+consumer_key = "TTSp8LeRV8P6w2xYStRHqeFPs"
+consumer_secret = "roUMN1Qh6Duh15JvbJMYtoe0SY7kMXTYRiF5AsBBUnKpwvz0Rn"
+access_token = "879512133557420032-3aeJKPHUYIX1FG4ilzW7zJcLZXQP6cU"
+access_token_secret = "8IOy0mE027XHLoU9ZB5WBsGWCaMVsYfAOSJ0ZEWv202uf"
+
+#twitter atturatize
+client = tweepy.Client(consumer_key=consumer_key,
+                    consumer_secret=consumer_secret,
+                    access_token=access_token,
+                    access_token_secret=access_token_secret)
 # API endpoint
 url = "https://api.producthunt.com/v2/api/graphql/"
 
@@ -24,8 +35,8 @@ query = """
 
 """
 
-# API request
-response = requests.post(url, headers={"Authorization": f"Bearer {access_token}"}, json={"query": query})
+# API request producthunt
+response = requests.post(url, headers={"Authorization": f"Bearer {access_token_producthunt}"}, json={"query": query})
 
 # Check if request was successful
 if response.ok:
@@ -35,6 +46,7 @@ if response.ok:
   # Process or use data
   for posts in data:
     node = posts.get("node", {})
+    tweet = client.create_tweet(text=node.get("name"))
     print(node.get("name"), node.get("tagline"), node.get("votesCount"))
 else:
   # Handle error
